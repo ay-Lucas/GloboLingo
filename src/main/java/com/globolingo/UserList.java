eb944677e2d390aacd39382f5becff6138eaa11f
unknown <jcabrey@email.sc.edu>
2024-10-19 11:54:07 -0400

    update #14

--- src/main/java/com/globolingo/UserList.java
+++ src/main/java/com/globolingo/UserList.java
@@ -17,6 +17,7 @@ import org.json.simple.parser.ParseException;
 public class UserList {
     private ArrayList<User> users;
     private static UserList instance;
+
     /**
      * Private constructor to prevent external instantiation.
      * Initializes the list of users and loads them from the JSON file.
@@ -25,6 +26,7 @@ public class UserList {
         users = new ArrayList<>();
         loadUsersFromJSON();
     }
+
     /**
      * Retrieves the singleton instance of UserList.
      * 
@@ -36,6 +38,7 @@ public class UserList {
         }
         return instance;
     }
+
     /**
      * Adds a new user to the list with the provided details.
      * 
@@ -52,6 +55,7 @@ public class UserList {
         users.add(newUser);
         return newUser;
     }
+
     /**
      * Retrieves the list of all users.
      * 
@@ -60,6 +64,7 @@ public class UserList {
     public ArrayList<User> getUsers() {
         return this.users;
     }
+
     /**
      * Searches for a user by their username.
      * 
@@ -74,6 +79,7 @@ public class UserList {
         }
         return null;
     }
+
     /**
      * Searches for a user by their UUID.
      * 
@@ -88,30 +94,48 @@ public class UserList {
         }
         return null;
     }
+
     /**
      * Edits the details of an existing user.
      * 
      * @param user the user to be edited.
      */
     public void editUser(User user) {
-        // Implement user editing logic here if needed.
+        User existingUser = getUser(user.getUsername());
+        if (existingUser != null) {
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setLevel(user.getLevel());
+            existingUser.setAvatar(user.getAvatar());
+            existingUser.setCourse(user.getCourse());
+            saveUser(existingUser);
+        }
     }
+
     /**
      * Logs out the specified user.
      * 
      * @param user the user to be logged out.
      */
     public void logOutUser(User user) {
-        // Implement logout logic here if needed.
+        user.setCourse(null);
+        System.out.println("User " + user.getUsername() + " has been logged out.");
     }
+
     /**
      * Saves the specified user's details.
      * 
      * @param user the user whose details are to be saved.
      */
     public void saveUser(User user) {
-        // Implement saving user details logic here if needed.
+        int index = users.indexOf(getUser(user.getUsername()));
+        if (index != -1) {
+            users.set(index, user);
+        }
+        System.out.println("User " + user.getUsername() + " has been saved.");
     }
+
     /**
      * Adds an existing User object to the list.
      * 
@@ -120,6 +144,7 @@ public class UserList {
     public void addUser(User user) {
         users.add(user);
     }
+
     /**
      * Loads users from a JSON file and adds them to the user list.
      * Each user is created with the details provided in the JSON file.
