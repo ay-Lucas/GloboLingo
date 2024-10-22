883e24fe46bca43754c77095b2c9fa3179d70c74
unknown <jcabrey@email.sc.edu>
2024-10-22 13:02:50 -0400

    fixed #57

--- src/main/java/com/globolingo/Language.java
+++ src/main/java/com/globolingo/Language.java
@@ -1,8 +1,25 @@
 package com.globolingo;
 public enum Language  {
-    SPANISH;
+    SPANISH,
+    JAPANESE,
+    GERMAN,
+    ITALIAN,
+    FRENCH;
 
     public String getLanguage()  {
-        
+        switch (this) {
+            case SPANISH:
+                return "SPANISH";
+            case JAPANESE:
+                return "JAPANESE";
+            case GERMAN:
+                return "GERMAN";
+            case ITALIAN:
+                return "ITALIAN";
+            case FRENCH:
+                return "FRENCH";
+            default:
+                throw new IllegalArgumentException("Unknown language: " + this);
+        }
     }
 }
