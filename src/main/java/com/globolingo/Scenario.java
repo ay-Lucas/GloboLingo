647bb46971500046a67fe248fca7673c6d070b63
Ian Lecker <idlecker16@gmail.com>
2024-10-24 13:58:39 -0400

    commit

---
+++ src/main/java/com/globolingo/Scenario.java
@@ -0,0 +1,12 @@
+package com.globolingo;
+
+public class Scenario {
+    
+    private void scenario1() {
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Scenario 1");
+    }
+}
